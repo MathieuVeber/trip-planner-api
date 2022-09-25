b6b7be3a26accbebfa8461fa56143088cf927e17
Mathieu Veber <math.veber@gmail.com>
2022-09-25 11:23:50 +0200

    Run prettier

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import express from 'express'
 const server = express()
 
 server.listen(process.env.PORT, async () => {
-	console.log(`Trip Planner API listening on port ${process.env.PORT}`)
+  console.log(`Trip Planner API listening on port ${process.env.PORT}`)
 })
