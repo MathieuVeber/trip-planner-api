0f2a11a5b97398a4394817eb2c8208797ae6b16d
Mathieu Veber <math.veber@gmail.com>
2022-09-27 17:37:36 +0200

    Add health endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 
 const PORT = process.env.PORT || ''
 const server = express()
+
+server.get('/health', (req: Request, res: Response) => {
+  const data = {
+    uptime: process.uptime(),
+    currentTime: new Date(),
+  }
+
+  return res.status(200).send(data)
+})
 
 server.listen(PORT, () => {
   console.log(`Trip Planner API listening on port ${PORT}`)
