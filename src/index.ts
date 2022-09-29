f554ed878afcd201215adbb9691a7edf1a169350
Mathieu Veber <veberm8@gmail.com>
2022-09-29 21:19:42 +0200

    Use CORS

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express, { Request, Response } from 'express'
+import cors from 'cors'
 
 const PORT = process.env.PORT || ''
 const server = express()
+
+server.use(cors())
 
 server.get('/health', (req: Request, res: Response) => {
   const data = {
